804f8e5c3931854fe19126b5fb1fc96a7925c76a
Raman pawar <ramanpawar920@gmail.com>
2025-12-31 00:49:34 +0530

    start of react and vite library for java script

---
+++ REACT/basics_of_react_01/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Chai Aur Code | Raman pawar </h1>
+  );
+}
+
+export default App;
---
+++ REACT/my-app/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur Code | Raman pawar</h1>
+  )
+}
+
+export default App
